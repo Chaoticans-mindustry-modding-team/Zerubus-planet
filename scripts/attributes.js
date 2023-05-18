4c9dcff3e13538d156d7fe842869d52375ceacd1
Xaocsoul <128741371+Xaocsoul@users.noreply.github.com>
2023-05-17 18:46:46 -0600

    Update attributes.js

--- scripts/attributes.js
+++ scripts/attributes.js
@@ -1,9 +1,2 @@
 // Creates new attributes
-Attribute.add("wood");
-Attribute.add("stone");
-Attribute.add("sand");
-Attribute.add("iron");
-Attribute.add("zinc");
-Attribute.add("copper");
-Attribute.add("water");
 Attribute.add("silver");
