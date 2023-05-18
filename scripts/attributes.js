4065dfb61caced3e942bcaa4b0e43b22a1b1eb97
Xaocsoul <128741371+Xaocsoul@users.noreply.github.com>
2023-05-17 18:44:40 -0600

    Add files via upload

---
+++ scripts/attributes.js
@@ -0,0 +1,9 @@
+// Creates new attributes
+Attribute.add("wood");
+Attribute.add("stone");
+Attribute.add("sand");
+Attribute.add("iron");
+Attribute.add("zinc");
+Attribute.add("copper");
+Attribute.add("water");
+Attribute.add("silver");
